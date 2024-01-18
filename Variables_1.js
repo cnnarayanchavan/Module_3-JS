02d74d74d8dbc8a0c367608d44e60507e8cb946c
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-18 23:20:12 +0530

    variables in JS

---
+++ Variables_1.js
@@ -0,0 +1,8 @@
+console.log("Hello Narayana Chavan !!")
+let num = 2+2 
+console.log(num + 10)
+//code has to be convert into machine understandable language 
+let username = 'Nrayana'
+console.log(username)
+num = 1000
+console.log(num)
