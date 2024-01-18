cb7eb04dac187f051ebc1cab60e542838286a607
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-18 23:25:18 +0530

    Datatype-1 in js

---
+++ datatype_3.js
@@ -0,0 +1,8 @@
+let num = 123423456
+let username = "Narayana Chavan"
+let a  = 1
+//console.log(typeof username)
+//console.log(num * a)
+let N = -5/10
+console.log(Number.MAX_VALUE * a)
+console.log(N)
