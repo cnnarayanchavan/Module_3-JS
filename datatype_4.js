79e3a531969b5ef66374214a489d558d8ebcd277
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-18 23:26:15 +0530

    Datatypes-2 in js

---
+++ datatype_4.js
@@ -0,0 +1,9 @@
+let username = "Narayan"
+let channel_name = 'Narayanas "ASMR" '
+let name = "N\va\vr\va\vy\va\vn\va"
+//console.log(username + " " +  channel_name + " " + name)
+//console.log(name)
+
+let b = 6>7
+console.log(b)
+console.log(typeof b)
