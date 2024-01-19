9247430ba0908a3ba106bf906858de52f549a9f7
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-19 09:59:00 +0530

    Type Converion in Javascript

---
+++ type_conversion5.js
@@ -0,0 +1,12 @@
+let num 
+console.log(num)
+num = 10
+console.log(num , typeof num)
+
+//Type Conversion
+
+num = String(num) 
+console.log(typeof num)
+
+num = Number(num)
+console.log( typeof num)
