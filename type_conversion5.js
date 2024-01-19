87e16b4dd7bf919a708889adfec26a94f897a5e6
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-19 10:18:39 +0530

    print NAN and 'parseInt'

--- type_conversion5.js
+++ type_conversion5.js
@@ -10,3 +10,12 @@ console.log(typeof num)
 
 num = Number(num)
 console.log( typeof num)
+
+
+//LATES PRINT NAN
+let a = Number("123 Narayana")
+console.log(a)
+
+//ok lets force print interger value from string 
+a = parseInt("123 NArayana")
+console.log(a)
