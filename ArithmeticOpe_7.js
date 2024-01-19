a0cb34e7bc30d5135a38fc02ed9564dc078636ed
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-19 23:56:42 +0530

    Arithmethic operations in JavaScript

---
+++ ArithmeticOpe_7.js
@@ -0,0 +1,20 @@
+let n1 = true
+let n2 = true
+let result = n1 + n2
+console.log(result)
+
+let num = 8
+++num
+num++
+console.log(num)
+
+// inbuilt class for calculating square of any number 
+let square = 2
+let res = Math.pow(2,2)
+console.log(res)
+
+//here's  a new advance trick for calculating square,cubes and others powers
+let powers = 9**2
+console.log(powers)
+
+
