ec4cb943a0fbc98f9f187cadb8a9a668ad611f52
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-20 11:50:37 +0530

    Update logical AND,OR,NOT LogicalOpe.js

--- LogicalOpe.js
+++ LogicalOpe.js
@@ -4,9 +4,20 @@
 //NOT   {!}
 //OPERATIONS
 
-
+//AND {&&}
 let a = 10 
 let b = 20
 let c = 30
 let res = a>b && a>c
 console.log(res)
+
+//OR{|}
+res = a<b | b>c
+console.log(res)
+
+// NOT{!}
+//lets check the  Logical 'NOT' operator
+let d = !res
+console.log(d)
+
+
