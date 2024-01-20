d807cbe847cf103fc92424478b12ec53df15752d
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-20 09:18:23 +0530

    Update RelationalOpe_7.js_ (compared Strings)

---
+++ RelationalOpe_7.js
@@ -0,0 +1,7 @@
+//lets talk abt relation operator 
+
+//HERE IT IS ACTUALLY THE COMPARISION BETWEEN TOW STRINGS WHEATHER IT IS GREATER OR LESSER OF EACH OTHER
+let a = "Narayana"
+let b = "Chavan"
+let res = a>b
+consol.log(res)
