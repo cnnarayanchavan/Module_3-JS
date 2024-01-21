dfabdbf61b984dae4239c59610cd8d4e48675398
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-21 13:50:38 +0530

    Switch Stme in js

---
+++ SwitchStmt_11.js
@@ -0,0 +1,24 @@
+let Assassians;
+switch(Assassians)
+  case "Press1" :
+      console.log("equpe hand gun")
+      break;
+  case "press2":
+      console.log("eqipe blades");
+      break;
+  case "press3":
+      console.log("eqipe swords");
+      break;
+  case "press4":
+      console.log("eqipe smoke bombs");
+      break;
+  case "press5":
+      console.log("eqipe rope dart");
+      break;
+  default:
+      console.log("Keep fighting with ur wrist")
+        
+        
+
+      
+      
