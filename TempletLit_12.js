5c29f2a1583b8549413a779923d8237d7fb0cfd8
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-21 19:05:36 +0530

    Update TempletLit_12.js

--- TempletLit_12.js
+++ TempletLit_12.js
@@ -4,3 +4,7 @@ let b = 20;
 let c = 30;
 let add = a+b+c
 console.log ("the addation of the a , b ,c is:" +add)
+//SEE BUT NOW HERE INSTEAD OF WRITING LIKE THIS WITH USINS + HERE WE USE TEMPLET 
+// FOR THAT WE HAVE TO USE [` `] BACKTICK AND $ AND {}
+//HERE'S THE OUTPUT
+console.log(`The addition of number a,b,c is := ${add}`)
