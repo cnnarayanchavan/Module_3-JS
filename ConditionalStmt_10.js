e552df7546d87dff521ff8c2720f697c43321c25
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-21 11:23:01 +0530

    conditional statements

--- ConditionalStmt_10.js
+++ ConditionalStmt_10.js
@@ -1,10 +1,9 @@
 //lets start with the 'if else' conditononal statements.
 let x = 2 ;
 let y = 20 ;
-let result = x % y;
-if(result ==0 );
-  console.log("Even number");
+if( x > y)
+  console.log("true");
 else
-  console.log("Odd number");
+  console.log("false");
 
 //follow the concentions, it is a good practice
