633d66da7b22bae675df1b4434230b0f54085969
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-21 19:08:19 +0530

    Use of templet literal

--- TempletLit_12.js
+++ TempletLit_12.js
@@ -3,8 +3,12 @@ let a = 10;
 let b = 20;
 let c = 30;
 let add = a+b+c
+
+//way 1
 console.log ("the addation of the a , b ,c is:" +add)
 //SEE BUT NOW HERE INSTEAD OF WRITING LIKE THIS WITH USINS + HERE WE USE TEMPLET 
 // FOR THAT WE HAVE TO USE [` `] BACKTICK AND $ AND {}
 //HERE'S THE OUTPUT
+
+//way2
 console.log(`The addition of number a,b,c is := ${add}`)
