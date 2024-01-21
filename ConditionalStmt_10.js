611142afea5d17b11e7cfe82439cee7c56a0445a
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-21 11:31:01 +0530

    Update ConditionalStmt_10.js

--- ConditionalStmt_10.js
+++ ConditionalStmt_10.js
@@ -1,9 +1,13 @@
 //lets start with the 'if else' conditononal statements.
 let x = 2 ;
 let y = 20 ;
-if( x > y)
+if( x > y){
   console.log("true");
-else
+  consloe.log("woww")
+}
+else{
   console.log("false");
-
+  console.log("Alass!!!!")
+}
+//if the statement content more than one declearation the you have to add curly brackets
 //follow the concentions, it is a good practice
