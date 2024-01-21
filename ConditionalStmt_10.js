db33a5f72735f9cb215af6b39423f00e3011dcb5
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-21 11:11:21 +0530

    Conditonal if , else in javascript

---
+++ ConditionalStmt_10.js
@@ -0,0 +1,10 @@
+//lets start with the 'if else' conditononal statements.
+let x = 2 ;
+let y = 20 ;
+let result = x % y;
+if(result ==0 );
+  console.log("Even number");
+else
+  console.log("Odd number");
+
+//follow the concentions, it is a good practice
