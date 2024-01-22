929f56a25999def4380c56afc8ea3af1ebad6268
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-22 11:48:27 +0530

    nested for loop _Update ForLoop_15.js

--- ForLoop_15.js
+++ ForLoop_15.js
@@ -2,3 +2,11 @@
 
 for(let i = 1; i<=5; i++)
   console.log("HI" , i)
+
+//now here lets start the actual question with "Nested For loop"
+
+for(i=1; i<=5; i++){
+  for(j=1; j<=5; j++)
+    console.log(' "*" ')
+}
+console.log("\n")
