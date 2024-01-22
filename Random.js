7c58cb9eff593041cd38ed5ee9ae3daa1ee80837
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-22 08:33:16 +0530

    Update Random.js

--- Random.js
+++ Random.js
@@ -2,15 +2,20 @@
 
 //here number is prime or not 
 
-let number = "Narayana"  //lets get a wrong statment here to see how backtick' ` ' work 
-//find number is prime or not
-//prime is what the number which is divisible by one and thet number 
-if(number%2===0)
-  console.log("it is even")
-else if(number ** 2 === 144)
-  console.log("square is correct")
-else
-  console.log(`totallllyy wrong
-  Statement`)
-1
-  
+// let number = "Narayana"  //lets get a wrong statment here to see how backtick' ` ' work 
+// //find number is prime or not
+// //prime is what the number which is divisible by one and thet number 
+// if(number%2===0)
+//   console.log("it is even")
+// else if(number ** 2 === 144)
+//   console.log("square is correct")
+// else
+//   console.log(`totallllyy wrong
+//   Statement`)
+
+//now let's check some string questions 
+let username = 'Narayana Chavan from "RMDSSOE" '
+//here i just wnated to give double quote to the RMDSSOE college
+console.log(username)
+
+
