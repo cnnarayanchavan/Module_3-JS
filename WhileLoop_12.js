e18733d162b6700da19aedcb098e22c9e6e1480c
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-22 09:54:40 +0530

    Comment down unwanted changes

--- WhileLoop_12.js
+++ WhileLoop_12.js
@@ -28,35 +28,30 @@ while(i<=num){
     console.log('"It is prime number"')
   else
     console.log('"It is not a prime number"')
-
-
-//  THIS IS EXTRIME BROUT FIRST SOLUTON OF THIS QUESTION
-   
 }
+
+//  THIS IS EXTRIME BROUT FIRST SOLUTON OF THIS QUESTION 
+
 
 
 //now lets try something diff. with while loop
 //prime number: the number that can have exact two factors 1 and itself
 
-let num = 34
-let count = 0
-int i = 1;
-while(i<=num){
-  if(num % i ==0){
-  count++;
-  }
-  i++
-  if(count == 2)
-    console.log('"It is prime number"')
-  else
-    console.log('"It is not a prime number"')
+// let num = 34
+// let count = 0
+// int i = 1;
+// while(i<=num){
+//   if(num % i ==0){
+//   count++;
+//   }
+//   i++
+//   if(count == 2)
+//     console.log('"It is prime number"')
+//   else
+//     console.log('"It is not a prime number"')
 
 
-//  THIS IS EXTRIME BROUT FIRST SOLUTON OF THIS QUESTION
-  
-    
-
-  
-}
+// //  THIS IS EXTRIME BROUT FIRST SOLUTON OF THIS QUESTION
+// }
 
 console.log(i);
