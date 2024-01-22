ed29adb554af2b1470df015f7f8729dbe21020af
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-22 12:04:31 +0530

    Added the table using for loop

--- ForLoop_15.js
+++ ForLoop_15.js
@@ -1,12 +1,18 @@
 //lets print stars using for loop 
 
-for(let i = 1; i<=5; i++)
-  console.log("HI" , i)
+// for(let i = 1; i<=5; i++)
+//   console.log("HI" , i)
 
 //now here lets start the actual question with "Nested For loop"
 
-for(i=1; i<=5; i++){
-  for(j=1; j<=5; j++)
-    console.log(' "*" ')
+// for(i=1; i<=2; i++){
+//   for(j=1; j<=3; j++)
+//     console.log(' "*" ')
+// }
+// console.log("\n")
+
+let num = 19
+for(i=1; i<=10; i++){
+  var1 = num*i
+  console.log(`${num}X${i}=${var1}`);
 }
-console.log("\n")
