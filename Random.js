c6b5ce10cd52eaae8129d61e8468ee28c1798d5d
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-22 08:24:21 +0530

    Just a random practices of basis javascript

--- Random.js
+++ Random.js
@@ -1,1 +1,16 @@
+//let's practice basic jsvascript for next 20 mins
 
+//here number is prime or not 
+
+let number = 12
+//find number is prime or not
+//prime is what the number which is divisible by one and thet number 
+if(number%2===0)
+  console.log("it is even")
+else if(number ** 2 === 144)
+  console.log("square is correct")
+else
+  console.log(`totallllyy wrong
+  Statement`)
+
+  
