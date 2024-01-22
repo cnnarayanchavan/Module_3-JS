ebea38225be8af6f79ceb8fa25fbaf6f643929c9
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-22 09:58:38 +0530

    Changed agai,  the commented area remove

--- WhileLoop_12.js
+++ WhileLoop_12.js
@@ -4,11 +4,12 @@
 
 //LETS understand while loop
 
-let i =1
-while(i<=5){
-  console.log('Hello Narayana "From RMD" ');
+ let i =1
+ while(i<=5){
+   console.log('Hello Narayana "From RMD" ');
   i++;
-}
+ }
+ console.log(i);
 
 //now the question what will be the value of i
 
@@ -18,40 +19,16 @@ while(i<=5){
 
 let num = 34
 let count = 0
-int i = 1;
+i = 1;
 while(i<=num){
   if(num % i ==0){
   count++;
   }
   i++
+}
   if(count == 2)
     console.log('"It is prime number"')
   else
     console.log('"It is not a prime number"')
-}
 
 //  THIS IS EXTRIME BROUT FIRST SOLUTON OF THIS QUESTION 
-
-
-
-//now lets try something diff. with while loop
-//prime number: the number that can have exact two factors 1 and itself
-
-// let num = 34
-// let count = 0
-// int i = 1;
-// while(i<=num){
-//   if(num % i ==0){
-//   count++;
-//   }
-//   i++
-//   if(count == 2)
-//     console.log('"It is prime number"')
-//   else
-//     console.log('"It is not a prime number"')
-
-
-// //  THIS IS EXTRIME BROUT FIRST SOLUTON OF THIS QUESTION
-// }
-
-console.log(i);
