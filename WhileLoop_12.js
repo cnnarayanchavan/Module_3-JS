91adb29b4f6018d8dc0f8db6e336501766ab42d5
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-22 09:06:06 +0530

    looping statement in JS Update WhileLoop_12.js

---
+++ WhileLoop_12.js
@@ -0,0 +1,13 @@
+// Initilization 
+//test condition 
+//incriment and decriment operator 
+
+//LETS understand while loop
+
+let i =1
+while(i<=5){
+  console.log('Hello Narayana "From RMD" ');
+  i++;
+}
+
+//now the question what will be the value of i
