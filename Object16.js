0f862a493721075eb06f8db0ecaa81453af66c73
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-23 13:49:09 +0530

    Object in javascript

---
+++ Object16.js
@@ -0,0 +1,10 @@
+let Obj1_of_Narayan = {
+    name: 'Narayaaaa',
+    age: 21,
+    lastname: 'chavan',
+    inestment: 8000
+}
+
+console.log(Obj1_of_Narayan);
+
+//here we can use . operator to specify the value that we want to fetch
