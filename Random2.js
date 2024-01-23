4ab6090b7e3f7f532cb72dd4b848c0ad896a5e62
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-23 13:39:44 +0530

    added

--- Random2.js
+++ Random2.js
@@ -5,7 +5,7 @@ let num = 12345
 while(num>0){
   num = parseInt(num / 10)
   lastdigit = parseInt(num%10)
-  console.log( "reverse of the digits are: " lastdigit )
+  console.log( "reverse of the digits are: " +lastdigit )
 }
 
 
