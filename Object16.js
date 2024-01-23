4aa6f23d8d49aa3ab5e6cdbe9ae592dba8fcd4a4
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-23 16:25:43 +0530

    Update Object16.js

--- Object16.js
+++ Object16.js
@@ -1,10 +1,31 @@
-let Obj1_of_Narayan = {
-    name: 'Narayaaaa',
-    age: 21,
-    lastname: 'chavan',
-    inestment: 8000
-}
+// let Obj1_of_Narayan = {
+//     name: 'Narayaaaa',
+//     age: 21,
+//     lastname: 'chavan',
+//     inestment: 8000
+// }
 
-console.log(Obj1_of_Narayan);
+// console.log(Obj1_of_Narayan);
 
 //here we can use . operator to specify the value that we want to fetch
+
+
+
+
+//now let's try something new
+//with new type of object in JavaScript
+let Alian = {
+    name: 'altos'
+     age: 1900
+type : 'harvivorus'
+gender:'mail'
+speceship = {
+    TopSpeed : 12000
+    wheels : 2
+booster :'altrapro'
+}
+}
+//this is nothing but the conplext object
+console.log(Alian.spaceship.wheels);
+
+
