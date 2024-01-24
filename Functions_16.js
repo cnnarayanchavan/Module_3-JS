d860a7b5a89e63307281fcbb0f8e7d55eb5c540c
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-24 21:55:47 +0530

    conplex functions in js

--- Functions_16.js
+++ Functions_16.js
@@ -7,3 +7,32 @@ function add(){
   console.log(c);
 }
 add();
+
+//now lets gain about the return and passing in functions
+
+// here the function without name is anonymous
+
+function calculator(){
+  let num1 
+  let num2
+  let operations
+  switch(operations){
+      case: 'addi' 
+      console.log(num1+num2)
+      break;
+      case: 'subs'
+      console.log(num1-num2)
+      break;
+      case: 'div'
+      console.log(num1/num2)
+      break;
+      case: 'multi'
+      console.log(num1*num2)
+      break;
+    default:
+      console.log('invalid input')
+  }
+}
+
+let result = calculator(3,3)
+console.log(result)
