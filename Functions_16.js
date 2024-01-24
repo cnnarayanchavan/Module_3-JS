93e82963495dbaa01a2f0afe6a247e5caf59cc51
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-24 21:46:01 +0530

    Added

--- Functions_16.js
+++ Functions_16.js
@@ -1,8 +1,9 @@
 //HERE WE ARE LEARNING ABT THE FUNCTIONS IN JS
 //FUNCTIONS IN JS IS PREEY SIMILAR TO THE FUNCTIONS IN C(PROGRAMMING LANGUAGE)
 function add(){
-  let num1= 12
-  let num2 =12
+  let a=90;
+  let b=99;
+  let c =a+b;
+  console.log(c);
 }
-let result = add()
-console.log(result)
+add();
