331215a7259b6b333667b46add7267c26d0f0f40
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-24 21:58:58 +0530

    updated

--- Functions_16.js
+++ Functions_16.js
@@ -1,14 +1,13 @@
 //HERE WE ARE LEARNING ABT THE FUNCTIONS IN JS
 //FUNCTIONS IN JS IS PREEY SIMILAR TO THE FUNCTIONS IN C(PROGRAMMING LANGUAGE)
-function add(){
-  let a=90;
-  let b=99;
-  let c =a+b;
-  console.log(c);
-}
-add();
+// function add(){
+//   let a=90;
+//   let b=99;
+//   let c =a+b;
+//   console.log(c);
+// }
+// add();
 
-//now lets gain about the return and passing in functions
 
 // here the function without name is anonymous
 
@@ -17,16 +16,16 @@ function calculator(){
   let num2
   let operations
   switch(operations){
-      case: 'addi' 
+      case 1: 'addi' 
       console.log(num1+num2)
       break;
-      case: 'subs'
+      case 2: 'subs'
       console.log(num1-num2)
       break;
-      case: 'div'
+      case 3: 'div'
       console.log(num1/num2)
       break;
-      case: 'multi'
+      case 4: 'multi'
       console.log(num1*num2)
       break;
     default:
