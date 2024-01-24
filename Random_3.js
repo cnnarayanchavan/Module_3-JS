4a7455f6bce0a536fc4f61f5b885f1e1ab7f9bbe
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-24 09:08:00 +0530

    Create Random_3.js_UPDATED

---
+++ Random_3.js
@@ -0,0 +1,12 @@
+//lets revise the object and complex objects in javascript
+let obj_1_Car = {
+  Company_name:TaTa,
+  Top_speed:233,
+  Wheels:8,
+  Price:546000,
+  Tata_Punch:{
+    Weignt:230,
+    speed:500,
+    RPM:2300,
+  }
+}
