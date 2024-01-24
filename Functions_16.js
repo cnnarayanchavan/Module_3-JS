a69543a98d4ad685973442c98cc640cb4651d89b
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-24 15:26:26 +0530

    functions in JavaScript Create Functions_16.js

---
+++ Functions_16.js
@@ -0,0 +1,8 @@
+//HERE WE ARE LEARNING ABT THE FUNCTIONS IN JS
+//FUNCTIONS IN JS IS PREEY SIMILAR TO THE FUNCTIONS IN C(PROGRAMMING LANGUAGE)
+function add(){
+  let num1= 12
+  let num2 =12
+}
+let result = add()
+console.log(result)
