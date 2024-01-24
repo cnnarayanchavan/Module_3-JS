e08f1a4275ebae33e15e227c68e6e68e03129c8b
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-24 09:27:11 +0530

    Added the for in loop and complex onjects

--- Random_3.js
+++ Random_3.js
@@ -12,9 +12,5 @@ let obj_1_Car = {
 }
 //console.log(obj_1_Car);
 for(let key in obj_1_Car){
-<<<<<<< HEAD
   console.log(key,obj_1_Car[key])
-=======
-  console.log(key,Obj_1_Car[key])
->>>>>>> e821189a5b7e228b5d64d9c61d5073212b3b8216
 }
