5422ae792e1a2b7b57fb80223b9171335f537e85
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-25 23:44:45 +0530

    random5.1 (extraction of digits)

---
+++ Random5.js
@@ -0,0 +1,12 @@
+//in this file there will be only revision part of past few days and the reevision session 
+// and more of the comments and some questions 
+
+//lets extract the numbers from digits
+let num = 1124
+let count = 0
+while(num>0){
+  num = num / 10;
+  count++
+}
+console.log(`total digits in number is ${count} `);
+
