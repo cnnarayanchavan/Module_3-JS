e8799868d1e739d4088f06662e9226bd5c10f6ac
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-26 17:49:16 +0530

    Update Function17.js

--- Function17.js
+++ Function17.js
@@ -1,2 +1,11 @@
 //lets set some example using function in js
 //understand //functions //functions passing nad argumnets  // function return type
+function gTA(Key){
+  if(Key = 'f' && key = 'enter')
+    console.log('open door of objects')
+  else
+    console.log('exit from car')
+}
+let press = gTA('f')
+console.log(press)
+
