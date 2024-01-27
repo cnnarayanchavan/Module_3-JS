91cdb9c90eb73d19f61137bee6f6a7573ab69411
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-27 11:54:34 +0530

    Differnect betwwen the normal function and the arrow function

--- Arrow18.js
+++ Arrow18.js
@@ -5,3 +5,13 @@ let add = (n1 , n2) => {
 }
 let result = add(3,2)
 console.log(result)
+
+
+// now lets see the differenct between the normal function and the arrow function in js
+// normal function 
+let x = function (num1, num2){   //here this is the function without name wich is called 'Anonymous' function 
+    return num1 + num2;
+}
+let z = x(100,100)
+console.log(x)
+
