81a769db21f5c4cdb996df4fbe523408fb9470c5
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-27 15:51:32 +0530

    methods in the js

---
+++ Methods18.js
@@ -0,0 +1,16 @@
+//lets understand how methods works in js
+//we know that the methods are created inside the object(instance of an class)
+
+let Laptop = {
+  Company : 'MSIgf63',
+  processor : 'i5',
+  Generatiomn : '9th',
+  Ram : '16gb',
+  function Processor =>{
+  console.log("High processor with intel i5 9th gen engine additionaly it comes with tow thermal pests")
+  }  
+}
+for(let key in Laptop){
+  console.log(key , Laptop[kary])
+}
+processor()
