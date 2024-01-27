091bd95ae2aa74f90565ddc8d5f82ee95a0f9027
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-27 11:46:17 +0530

    Addded

--- Function17.js
+++ Function17.js
@@ -1,11 +1,24 @@
 //lets set some example using function in js
 //understand //functions //functions passing nad argumnets  // function return type
 function gTA(Key){
-  if(Key = 'f' && key = 'enter')
+  if(Key = 'f' && 'enter')
     console.log('open door of objects')
   else
     console.log('exit from car')
 }
-let press = gTA('f')
-console.log(press)
+gTA('f')
 
+function part2(one,two,three,four,five){
+   mobilenumber = one+two+three+four+five
+   return mobilenumber
+}
+let result = part2(97,45,23,45,67)
+console.log(result);
+
+
+
+function printsum(num, power ){
+  return num**power
+}
+let d = printsum(12,2)
+console.log(d);
