ab95b52e943b78f4f611e5eed41a5c18566db5d6
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-28 00:55:33 +0530

    Update This18.js

---
+++ This18.js
@@ -0,0 +1,10 @@
+//Use of this in js
+
+let obj1 ={
+  firstname:'narayan',
+  lastname:'chavan',
+  age:21,
+  prn:1234,
+  greet:function()=> 'hey ${this.firstname} ${this.lastname} welcome to communaty and please enter your ${this.age}'
+}
+obj1.greet();
