f46cd8616adb9cfb3a7c57498e9ad3ac92e4ee71
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-28 01:02:08 +0530

    Update This18.js

--- This18.js
+++ This18.js
@@ -5,7 +5,12 @@ let obj1 ={
   lastname:'chavan',
   age:21,
   prn:1234,
-  greet:function() => console.log('hey ${this.firstname} ${this.lastname} welcome to communaty and please enter your ${this.age}')
+  //greet:function() => console.log('hey ${this.firstname} ${this.lastname} welcome to communaty and please enter your ${this.age}')
+  greet:function(){
+    console.log('hey ${this.firstname} ${this.lastname} welcome to communaty and please enter your ${this.age}'
+  }
 }
 obj1.greet();
+
+//so from the tast cases we got to know that we cannnot use arrow function in objects for creating methods 
 
