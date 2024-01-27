d0f13bda93cb1f2893ba04cfaeb0d340dc5a84f8
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-28 01:06:01 +0530

    Update This18.js

--- This18.js
+++ This18.js
@@ -8,7 +8,7 @@ let obj1 ={
   //greet:function() => console.log('hey ${this.firstname} ${this.lastname} welcome to communaty and please enter your ${this.age}')
   //here we have to use the back tick for showing properties in the function 
   greet:function(){
-    console.log(`hey ${this.firstname} ${this.lastname} welcome to communaty and please enter your ${this.age}`)
+    console.log(`hey ${this.firstname} ${this.lastname} welcome to community and are you ${this.age} years old?`)
   }
 }
 obj1.greet();
