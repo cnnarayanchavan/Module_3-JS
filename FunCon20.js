ce62db87c8ea909f46463f52a6a0a3dd5f26db7c
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-29 12:04:17 +0530

    Update FunCon20.js

--- FunCon20.js
+++ FunCon20.js
@@ -1,7 +1,7 @@
 //today we learnt new method of printing in javascript
 let firstname ='Narayana';
 let Lastname ='Chavan';
-const LogineID ='cnnarayanchavan';
+const LoginID ='cnnarayanchavan';
 var Email='cnnarayanchavan@gmail.com';
 console.table([firstname,Lastname,LoginID,Email]);
 
