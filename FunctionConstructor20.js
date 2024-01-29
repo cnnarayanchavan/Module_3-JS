66328503a21f7c2db3b9241df2196c8e5d93d52b
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-29 11:54:31 +0530

    new printing method in JS

---
+++ FunctionConstructor20.js
@@ -0,0 +1,7 @@
+//today we learnt new method of printing in javascript
+let fisrtname ='Narayana';
+let Lastname ='Chavan';
+const LogineID ='cnnarayanchavan';
+var Email='cnnarayanchavan@gmail.com';
+conple.table([firstname,Lastname,LoginID,Email]);
+
