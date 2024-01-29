7b87622a2a838cdb1b360bc5838be5271da8444d
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-29 12:43:12 +0530

    revision of constructorfunction in js

---
+++ Random6.js
@@ -0,0 +1,9 @@
+//let's revise the last topic of js which is FunctionConstructor in js
+//let's create a function first
+
+let Narayan = function(Firstname,Lastname){  //this is called anonamous function means function without name
+  this.firstname = Firstname;
+  this.lastname = Lastname;
+  this.greet = function => console.table([Firstname,Lastname]);
+}
+let obj1 = new Narayan('Narya','Chavan');
