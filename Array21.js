d676e00e21d4194ef467700aa7929e1c0f31803a
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-29 14:58:30 +0530

    Arrays in javascript (declearation, fetching and adding values in it)

---
+++ Array21.js
@@ -0,0 +1,11 @@
+//lets understand object type 'Arrays' taa daaa
+let Arr1 = new Array(); //this is one type of creating an array 
+let arr1 = [];         // and this is another type of creating an arrays in js
+
+//so here we will fo with 1st method
+
+Arr1.push(1);
+Arr1.puch(1);
+Arr1.puch(2);
+Arr1.puch(4);
+console.log(Arr1);
