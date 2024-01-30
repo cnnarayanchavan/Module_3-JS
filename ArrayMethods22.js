3491a3b57962072b0db74e6d9e0af5d45db50625
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-01-30 10:57:31 +0530

    Create ArrayMethods22.js

---
+++ ArrayMethods22.js
@@ -0,0 +1,16 @@
+//lets see the differenct methods of an array we have studide in this lectres
+//1] .push();    +++ append (add an element) at the end of the array
+
+//2] .pop();     +++ pop oot the element(last element) like (FILO) from an array
+
+//3] .shift();   +++ shift the element of an array to the LHS (it will change the index number of an elements of an array)
+
+//4] .unshift(); +++ shift the element of an array to the RHS (unshifting)
+
+//6] .split();   +++ slpits an values of an variables in array form (values could be NUmber, String etc)
+
+
+let Arr1 = [];  //empty array
+Arr1.push(1,2,3,4,5,6,7,8,9,10);
+console.log(Arr1);
+  
