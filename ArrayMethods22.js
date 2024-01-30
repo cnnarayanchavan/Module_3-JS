c21e0cbc3f77affdadce8e8cfdf9182fb40d675b
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-30 11:15:34 +0530

    Added

--- ArrayMethods22.js
+++ ArrayMethods22.js
@@ -7,6 +7,8 @@
 
 //4] .unshift(); +++ shift the element of an array to the RHS (unshifting)
 
+//5] .splic();   +++ 
+
 //6] .split();   +++ slpits an values of an variables in array form (values could be NUmber, String etc)
 
 
@@ -19,8 +21,16 @@ Arr1.pop(6);
 Arr1.pop(7);
 Arr1.pop(8);
 Arr1.pop(9);
+console.log("This is the Arr1");
 console.log(Arr1);
 
+//now lets shift some elemetnts to left and right 
+Arr1.shift(); 
+console.log('This is the Arr1 after shift the arr1');              //it will remove element from the start of an array
+console.log(Arr1);          // it works as a pop but it will pop element from start of an array
 
+Arr1.unshift(1);    
+console.log('And this is the arr1 after adding element from start');         //on the othe hand the unshift will add an element in array from the start 
+console.log(Arr1);
 
 
