338bfc9b93672ec56df82cd5c6225cc351172b41
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-01-31 23:53:54 +0530

    Added

--- Random7.js
+++ Random7.js
@@ -1,3 +1,8 @@
-let arr1 = [53,65,73,98,45,09];
-arr1.forEach((num)=> console.log(num * 2));
+let arr1 = [53,65,73,98,45,90];
+
+arr1.forEach((num)=> {
+    if(num % 2 ===0){
+        console.log(num**2)
+    }
+});
   
