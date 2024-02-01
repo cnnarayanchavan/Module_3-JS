2ae3a845c63b4dfbedfd614a235a520c2d294eb3
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-01 14:06:07 +0530

    learnt about MapReduce higher order functions in Array26.js

--- MapReduceArray26.js
+++ MapReduceArray26.js
@@ -1,4 +1,15 @@
 //lets understand the map reducr higher order functiions in array
 let array = new Array();
-array.push(2,4,6,5,7);
+array.push(2,4,6,5,7,11,78,77);
 console.log(array);
+//now what i want to do here is 
+//1st filter() it
+//2nd map() it
+//3rd reducr() it
+//he he he he 
+array.filter(n=>(n%2!=0)){
+  array.map(n*2);
+  forEach(n=>{
+    console.log(n)
+  })
+}
