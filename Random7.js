43aa40be5fba01be2a7893768ad02393f0c32514
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-01 13:21:18 +0530

    Revised for off loop as well with the forEach() method

--- Random7.js
+++ Random7.js
@@ -3,8 +3,15 @@
 let arr1 = [53,65,73,98,45,90];
 
 arr1.forEach((num)=> {
-    if(num % 2 ===0){
+    //if(num % 2 ===0){
         console.log(num**2)
     }
 });
   
+//OKAY here the for rach and the for of loop of aaray does the samw work UKKK//!!!
+//let's try and revise the for off loop in js.
+
+let A =[10,20,30,40,50];
+for(let n of A){
+    console.log(A);
+}
