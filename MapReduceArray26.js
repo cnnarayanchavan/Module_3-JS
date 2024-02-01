ee4456b1b7cbd5afbe1e3ef5dfceb4ce6d348bb2
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-01 14:25:10 +0530

    learnt about MapReduce higher order functions in Array26.js

--- MapReduceArray26.js
+++ MapReduceArray26.js
@@ -7,8 +7,9 @@ console.log(array);
 //2nd map() it
 //3rd reducr() it
 //he he he he 
-array.filter(n => n%2!=0)
-  array.map(n*2);
-  array.forEach(n=>{
-    console.log(n)
-  })
+let res =array.filter(n => n%2!=0)
+    .map(n => n+2)  // i dont understand why do sw use the array fucntion here again and again
+    .reduce((a,b) => a+b);
+    console.log(res);
+
+
