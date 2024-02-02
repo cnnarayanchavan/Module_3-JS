e51624532190a343ebf5440021af45c30693d373
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-02 11:50:33 +0530

    Update Random9.js

--- Random9.js
+++ Random9.js
@@ -1,4 +1,5 @@
 //lets see the examples of the Higher order function which is 
+//Question on MapReduce method in array in JS
 // Filter
 // Map
 // Reduce
@@ -9,4 +10,4 @@ let Arr = [User1={Firstname:'Narayan',Lastname:'Chavan',Age:21,Gender:'Male'},
           User3={Firstname:'Sanjay',Lastname:'Chavan',Age:50,Gender:'Male'},
           User1={Firstname:'Renuka',Lastname:'Dalwe',Age:27,Gender:'female'},
           User1={Firstname:'Bharati',Lastname:'Chavan',Age:48,Gender:'femal'},]
-console.log(Arr.length());
+console.log(Arr.length);
