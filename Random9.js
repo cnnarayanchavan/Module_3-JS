fecbfcc042f16e86b2c8c34919981ed8f53ee274
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-02 11:49:33 +0530

    Question on MapReduce method in array in JS

---
+++ Random9.js
@@ -0,0 +1,12 @@
+//lets see the examples of the Higher order function which is 
+// Filter
+// Map
+// Reduce
+
+//let Arr = [1,13,5,13,26,20]
+let Arr = [User1={Firstname:'Narayan',Lastname:'Chavan',Age:21,Gender:'Male'},
+          User2={Firstname:'Nitin',Lastname:'Chavan',Age:30,Gender:'Male'},
+          User3={Firstname:'Sanjay',Lastname:'Chavan',Age:50,Gender:'Male'},
+          User1={Firstname:'Renuka',Lastname:'Dalwe',Age:27,Gender:'female'},
+          User1={Firstname:'Bharati',Lastname:'Chavan',Age:48,Gender:'femal'},]
+console.log(Arr.length());
