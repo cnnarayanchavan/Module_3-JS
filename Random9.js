8bfa78b4980056f1ce15d02a12d2f4f8d1e9ead9
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-02 16:50:02 +0530

    solved example on array methode  'MAp and reduce'

--- Random9.js
+++ Random9.js
@@ -1,20 +1,12 @@
-//lets see the examples of the Higher order function which is 
-//Question on MapReduce method in array in JS
-// Filter
-// Map
-// Reduce
+let Arr = [{Firstname:'Narayan',Lastname:'Chavan',Age:21,Gender:'Male'},
+          {Firstname:'Nitin',Lastname:'Chavan',Age:30,Gender:'Male'},
+          {Firstname:'Sanjay',Lastname:'Chavan',Age:50,Gender:'Male'},
+          {Firstname:'Renuka',Lastname:'Dalwe',Age:27,Gender:'female'},
+          {Firstname:'Bharati',Lastname:'Chavan',Age:48,Gender:'femal'}]
 
-//let Arr = [1,13,5,13,26,20]
-//First problem Statement : fetch only the male user's data
-//second problem statement : fetch only Username of male users
-let Arr = [User1={Firstname:'Narayan',Lastname:'Chavan',Age:21,Gender:'Male'},
-          User2={Firstname:'Nitin',Lastname:'Chavan',Age:30,Gender:'Male'},
-          User3={Firstname:'Sanjay',Lastname:'Chavan',Age:50,Gender:'Male'},
-          User4={Firstname:'Renuka',Lastname:'Dalwe',Age:27,Gender:'female'},
-          User5={Firstname:'Bharati',Lastname:'Chavan',Age:48,Gender:'femal'},]
-// console.log("This is the arr");          
-// console.log(Arr);
-//Arr.filter(Vare1=>User1.Gender=="Male")
-Arr.forEach(Vare2=>if(Vare2.Gender==='Male'));
-Arr.map(Vare1 => console.log(Vare1.Firstname + " " + Vare1.Lastname));
-
+          op = Arr.map(x => {
+            if (x.Age<30) {
+            console.log(x.Firstname)
+          }}
+          )
+        
