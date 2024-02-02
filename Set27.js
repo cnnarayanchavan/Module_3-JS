7429ae4193d546fe9ebddb928cdbc329cfdec976
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-02 17:11:14 +0530

    Learnt set in js

--- Set27.js
+++ Set27.js
@@ -2,5 +2,10 @@
 //it has unique values alongs with no repatation 
 let a = new Set("Narayana");
 console.log(a);
+//what if i want to add some more element in it 
 
-
+a.add(12);
+a.add(14);
+a.add(15);
+a.add(12);
+console.log(a)
