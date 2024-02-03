296355e78fca2c1cac37b2ec5439d1091042c654
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-03 13:19:06 +0530

    Learnt Map class in javascript.

--- Map28.js
+++ Map28.js
@@ -6,5 +6,11 @@ map.set("Narayan", 21);
 map.set("Nitin", 30);
 map.set("Bharati", 48);
 map.set("Sanjay", 50);
+//now here if we change the age of sanjay here it will update the value of it instead of adding in Map 
+map.set("Sanjay",52);
 console.log(map);
 
+// now let's use the forEach() higher order function in this Map 
+map.forEach(value => {
+    console.log(value)
+})
