cdf4bd791b13ec2097e597fcc6364ce27bfd67f6
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-03 13:25:51 +0530

    Sets in Js (intresting)

--- Set27.js
+++ Set27.js
@@ -1,7 +1,7 @@
 //lets under set keyword in js
 //it has unique values alongs with no repatation 
-let a = new Set("Narayana");
-console.log(a);
+let a = new Set();
+//console.log(a);
 //what if i want to add some more element in it 
 
 a.add(12);
@@ -10,10 +10,13 @@ a.add(15);
 a.add(12);
 console.log(a)
 
-let S = new Set();
-S.add(1);
-S.add(2);
-S.add(3);
-S.add(4);
-S.add(5);
-console.log(S);
+ console.log(a.has(123));
+
+
+// let S = new Set();
+// S.add(1);
+// S.add(2);
+// S.add(3);
+// S.add(4);
+// S.add(5);
+// console.log(S);
