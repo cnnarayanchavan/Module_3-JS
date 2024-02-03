b4de2391d9bb41154a9733494bd442b2da61accb
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-03 13:04:36 +0530

    Learnt Map in javascript.

---
+++ Map28.js
@@ -0,0 +1,10 @@
+// lets discuss the Map in js which is similar to object in js 
+//lets consider the example where you have a map with key and values (collection of data)
+let map = new Map();
+//we can directly add elements into the map or can use the "set" method for do the same 
+map.set("Narayan", 21);
+map.set("Nitin", 30);
+map.set("Bharati", 48);
+map.set("Sanjay", 50);
+console.log(map);
+
