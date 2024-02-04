91c8b6b7def5ec87196073cc5eb32db3847e316d
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-04 07:21:16 +0530

    Update higher_order_function.js

--- higher_order_function.js
+++ higher_order_function.js
@@ -3,9 +3,9 @@
 // map
 // reduce 
 
-//before higher order let's just see the functions in arrays
+//before higher order let's just see the functions in JS
 
-//const Arr = [1,24,9,20,5,96]
+//Fun1.
 function addition(num1 , num2){
   if(num1 === 20 && num2 ===30){
     return num1 + num2;
@@ -15,3 +15,18 @@ function addition(num1 , num2){
   }
 }
 console.log(addition(20,30))
+
+//Fun2.
+const Arr = [1,24,9,20,5,96]
+function returnEven(Arr){
+  for(i=0; i<Arr.length; i++){
+    if(i%2===0){
+      console.log(i);
+    }
+  }
+  return i;
+}
+const op = console.log(returnEven(Arr))
+console.log(op);
+
+
