c7d1f751898096467c00a883d627cb52e224d209
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-04 10:54:27 +0530

    Added new

--- Random9.js
+++ Random9.js
@@ -7,10 +7,3 @@ let Arr = [{Firstname:'Narayan',Lastname:'Chavan',Age:21,Gender:'Male'},
           const op =Arr.filter(x => x.Age<30)
 
           console.log(op)
-//         function firstName(Arr){
-//                   if(Arr.Age<30){
-//                     console.log(Arr.Firstname)
-//                   }
-//         }
-// let op =console.log(firstName(Arr));
-// console.log(op);
