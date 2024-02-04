047ba999aac8899dcc2d35d44585a700ec209649
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-04 10:51:51 +0530

    Update Random9.js

--- Random9.js
+++ Random9.js
@@ -4,15 +4,12 @@ let Arr = [{Firstname:'Narayan',Lastname:'Chavan',Age:21,Gender:'Male'},
           {Firstname:'Renuka',Lastname:'Dalwe',Age:27,Gender:'female'},
           {Firstname:'Bharati',Lastname:'Chavan',Age:48,Gender:'femal'}]
 
-          // op = Arr.map(x => {
-          //   if (x.Age<30) {
-          //   console.log(x.Firstname)
-          // }}
-          // )
-        function firstName(Arr){
-                  if(Arr.Age<30){
-                    console.log(Arr.Firstname)
-                  }
-        }
-let op =console.log(firstName(Arr));
-console.log(op);
+          Arr.filter(val => val.Age<30)
+console.log(val)
+//         function firstName(Arr){
+//                   if(Arr.Age<30){
+//                     console.log(Arr.Firstname)
+//                   }
+//         }
+// let op =console.log(firstName(Arr));
+// console.log(op);
