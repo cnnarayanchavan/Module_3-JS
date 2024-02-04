d318aad7146ebb75d5baea15580fa4f500d04731
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-04 10:57:26 +0530

    learnt map, reduce and filter higher order function

--- Random9.js
+++ Random9.js
@@ -4,4 +4,7 @@ let Arr = [{Firstname:'Narayan',Lastname:'Chavan',Age:21,Gender:'Male'},
           {Firstname:'Renuka',Lastname:'Dalwe',Age:27,Gender:'female'},
           {Firstname:'Bharati',Lastname:'Chavan',Age:48,Gender:'femal'}]
 var op = Arr.filter(x => x.Age<30)
-console.log(op)          
+//console.log(op)          
+//now apply map to fetch only first name of user whose age is less than 30
+.map(x => x.Firstname)
+console.log(op)         
