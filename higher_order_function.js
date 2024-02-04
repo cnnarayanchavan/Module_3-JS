0a3a5a67de469729fc3aef1b80487e0f6aa540ff
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-04 07:21:54 +0530

    Update higher_order_function.js

--- higher_order_function.js
+++ higher_order_function.js
@@ -6,15 +6,15 @@
 //before higher order let's just see the functions in JS
 
 //Fun1.
-function addition(num1 , num2){
-  if(num1 === 20 && num2 ===30){
-    return num1 + num2;
-  }
-  else{
-    console.log("Invalid Input")
-  }
-}
-console.log(addition(20,30))
+// function addition(num1 , num2){
+//   if(num1 === 20 && num2 ===30){
+//     return num1 + num2;
+//   }
+//   else{
+//     console.log("Invalid Input")
+//   }
+// }
+// console.log(addition(20,30))
 
 //Fun2.
 const Arr = [1,24,9,20,5,96]
