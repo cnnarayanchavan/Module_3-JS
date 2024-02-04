8b1f367078eb8e1cea811e491581d279069646b9
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-04 07:14:26 +0530

    Create higher_order_function.js

---
+++ higher_order_function.js
@@ -0,0 +1,17 @@
+//Let's revise the higner order function 
+// Filter
+// map
+// reduce 
+
+//before higher order let's just see the functions in arrays
+
+//const Arr = [1,24,9,20,5,96]
+function addition(num1 , num2){
+  if(num1 === 20 && num2 ===30){
+    return num1 + num2;
+  }
+  else{
+    console.log("Invalid Input")
+  }
+}
+console.log(addition(20,20))
