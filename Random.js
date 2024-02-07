1f2536ea08751b2da33767133793935a8197f01b
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-08 00:02:18 +0530

    Update Random.js

--- Random.js
+++ Random.js
@@ -30,4 +30,12 @@ while(number>0){
 
 
 //now lets fetch the max number inside the array
+const arr1 = [34,54,65,34,67,55];
+let max = 0;
+for(i=0; i<arr1.length; i++){
+  if(max<arr1){
+    max = arr1;
+  }
+}
+console.log(max)
 
