561ea5653a3f172364712d677670918dea00710a
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-08 00:16:47 +0530

    Max number

--- Random.js
+++ Random.js
@@ -30,9 +30,9 @@ while(number>0){
 
 
 //now lets fetch the max number inside the array
-const arr1 = [34,54,65,34,67,55];
-let max = 0;
-for(i=0; i<arr1[i].length; i++){
+let arr1 = [34,54,65,34,67,55]
+let max = 0
+for( i=0; i<arr1.length; i++){
   if(max<arr1[i]){
     max = arr1[i];
   }
