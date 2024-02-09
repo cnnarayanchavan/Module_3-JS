8ddc271517695684be74caabe8085f3cd9ee1479
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-09 14:00:57 +0530

    Update random10.js

--- random10.js
+++ random10.js
@@ -1,5 +1,8 @@
 //let's revise some jS concepts
 let arr =[5,5,5,5,5,10]
+let arr = [username : "Upgreadxlr8", id : 200, age: 21,
+           username : "TinTin", id : 110, age: 29,
+           username : "Xle8", id : 100, age: 22,]
 console.log(arr);
 
 //-----------------------------------------------------------------------------------------------//
@@ -25,12 +28,14 @@ console.log(arr);
 //-----------------------------------------------------------------------------------------------//
 //print the even number in arrays and total sum of even number
 //let arr =[5,5,5,5,5,10]
-let sum = 0;
-for(i=0; i<arr.length; i++){
-  if(arr[i]%2===0){
-    sum++;
-    console.log(arr[i])
-  }
-}
-console.log("Total number of even elements in array is : " +sum)
+// let sum = 0;
+// for(i=0; i<arr.length; i++){
+//   if(arr[i]%2===0){
+//     sum++;
+//     console.log(arr[i])
+//   }
+// }
+// console.log("Total number of even elements in array is : " +sum)
 
+
+//fetch the user data using the 
