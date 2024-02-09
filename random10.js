f0d3c4497ed1a86bea4d9598d8621dbc58d13ccd
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-09 13:46:52 +0530

    revised some concepts

--- random10.js
+++ random10.js
@@ -1,3 +1,12 @@
 //let's revise some jS concepts
 let arr =[1,3,45,6,4,5,67]
 console.log(arr);
+
+//print max number
+let max = 0;
+for(i = 0; i <arr.length; i++){
+    if(max = arr[i]){
+        max = arr[i]
+    }
+}
+console.log(`max element in array is: ${max}`);
