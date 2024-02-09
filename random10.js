b59eb325f589583bf92b2427834b20249728ea68
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-09 13:56:35 +0530

    Update random10.js

--- random10.js
+++ random10.js
@@ -4,6 +4,7 @@ console.log(arr);
 
 //-----------------------------------------------------------------------------------------------//
 // //print max number
+//let arr =[5,5,5,5,5,10]
 // let max = 0;
 // for(i = 0; i <arr.length; i++){
 //     if(max = arr[i]){
@@ -14,10 +15,22 @@ console.log(arr);
 
 //-----------------------------------------------------------------------------------------------//
 //print the total some of elements in array
+//let arr =[5,5,5,5,5,10]
+// let sum = 0;
+// for(i=0; i<arr.length; i++){
+//   sum = sum +arr[i]
+// }
+// console.log( "Total sum is: " +sum)
+
+//-----------------------------------------------------------------------------------------------//
+//print the even number in arrays and total sum of even number
+//let arr =[5,5,5,5,5,10]
 let sum = 0;
 for(i=0; i<arr.length; i++){
-  sum = sum +arr[i]
+  if(arr[i]%2===0){
+    sum++;
+    console.log(arr[i])
+  }
 }
-console.log( "Total sum is: " +sum)
-
+console.log("Total number of even elements in array is : " +sum)
 
