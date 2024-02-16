c0086f7aacce1eaea02c825fbecedfe743acc2c6
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-16 11:41:29 +0530

    01 rev js

--- Nc1.js
+++ Nc1.js
@@ -1,4 +1,4 @@
 // lets revise some of the concepts of js
 //1] ternary operator
 let num =20;
-let = (num % 2===0); console.log("Even") : console.log("Odd")
+let = (num % 2!=0)? console.log("Even") : console.log("Odd")
