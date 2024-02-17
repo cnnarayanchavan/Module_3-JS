92bb3abeeba49006c1d2c529ccf5a588bd38e6d7
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-17 13:16:38 +0530

    Create sorting.js_ sorting using logic of bubble sort in js.

---
+++ sorting.js
@@ -0,0 +1,15 @@
+// sorting in js
+
+let arr = [4,5,3,7,9,8];
+for(i=0; i<arr.length; i++){
+  for(j=0; i<arr.length-1-i; j++){
+    if(arr[j+1]<arr[j]){
+      temp = arr[j];
+      arr[j] = arr[j+1] 
+      arr[j+1] = temp
+    }
+  }
+  for(i=o; i<arr.length; i++){
+    console.log(arr[] + " ");
+  }
+}
