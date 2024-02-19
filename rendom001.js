c4c904279701f22ac4b86eb1d028ccffcbae16af
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-19 23:29:26 +0530

    Update rendom001.js

--- rendom001.js
+++ rendom001.js
@@ -2,7 +2,7 @@ let obj1 = {
   firstname : "narayana",
   lastname: "chavan", 
   age: 21,
-  id: 001,
+  id: 0_0_1,
 }
 // using for in loop we will fetch the keys from the obj_1
 for (key in obj1){
