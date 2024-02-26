f65af759ef4359120e9e4348e9a674869477c62c
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-26 17:01:27 +0530

    Update nc1.js_arrays in js

--- nc1.js
+++ nc1.js
@@ -1,6 +1,6 @@
 let str1 = "Hello Warld..!!"
 console.log(str1);
 let arr = [34,23,45,67,45,34,56,34]
-for(i =o; i<arr.length; i++){
+for( int i=o; i<arr.length; i++){
   console.log(arr[i] + " ")
 }
