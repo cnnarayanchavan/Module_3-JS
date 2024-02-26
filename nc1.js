f95cdb7ecb6830b5cc1bb390c77b752ffccfec5c
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-26 17:09:07 +0530

    Modified for each in file

--- nc1.js
+++ nc1.js
@@ -2,8 +2,10 @@ let str1 = "Hello Warld..!!"
 console.log(str1);
 let arr = [34,23,45,67,45,34,56,34]
 console.log(arr);
-foreach(n=>{
-  if(n%2==0){
+arr.forEach(element => {
+    
+});(element=>{
+  if(element%2==0){
     console.log(arr);
   }
 })
