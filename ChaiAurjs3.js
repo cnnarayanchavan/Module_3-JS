b5880bbc97254dc34e21dd7cbb751daa86dfbae6
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-05 12:39:53 +0530

    Update ChaiAurjs3.js_modified

--- ChaiAurjs3.js
+++ ChaiAurjs3.js
@@ -7,4 +7,4 @@ let obj1 = {
 let obj2 = obj1;
 obj2.email = "Cnnarayanchavan@gmail.com"
 console.log(obj1.email);
-console.log(obj2.email);
+console.log(obj2.email);  //so from that we can say that in non primetive data type we have pass by reference in which it will gives the original reference the new value
