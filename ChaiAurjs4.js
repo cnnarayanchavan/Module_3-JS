668c5bcd8981371cd72a41f1088b87464f3433e6
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-07 14:54:41 +0530

    date and time in js

---
+++ ChaiAurjs4.js
@@ -0,0 +1,8 @@
+let arr = [45,34,23,56,45,67];
+let element=56;
+for(i=0;i<arr.length;i++){
+    if (arr[i]===element) {
+        console.log(`element found at ${i} index`);
+    }
+}
+//console.log(arr);
--- ChaiAurjs5_Dates.js
+++ ChaiAurjs5_Dates.js
@@ -1,3 +1,17 @@
 let myDate = new Date();
-console.log(myDate);
-console.log(myDate.toString());
+// console.log(myDate);
+
+
+// console.log(myDate.toString());
+
+
+// console.log(myDate.toLocaleDateString());
+
+
+//console.log(myDate.getTime());
+
+
+//console.log(myDate.toISOString());
+
+console.log(myDate.getUTCMilliseconds());
+
