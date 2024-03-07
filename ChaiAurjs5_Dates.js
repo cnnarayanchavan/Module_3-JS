c24c3d8e935857e9a99a40aa2075f285d4d06b4f
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-07 15:14:44 +0530

    Unleash the date and time in jssss

--- ChaiAurjs5_Dates.js
+++ ChaiAurjs5_Dates.js
@@ -13,5 +13,17 @@ let myDate = new Date();
 
 //console.log(myDate.toISOString());
 
-console.log(myDate.getUTCMilliseconds());
+//console.log(myDate.getUTCMilliseconds());
 
+
+console.log(myDate.toISOString());
+console.log(myDate.getMonth());  //her as day starts from 0 it will print from 0 ,1 ,2, 3 ...
+// as a solution we use
+console.log(myDate.getMonth()+1);
+
+
+const fee = "5500 Pounds"
+let curDate = new Date();
+console.log(curDate);
+console.log(`Your registration fee is ${fee} and the date of registration is ${curDate.toLocaleDateString()}`);
+ 
