072861b96dac295961e3b9e803e35f1c27af099c
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-09 00:09:42 +0530

    Re revised some cooncepts in js

--- ChaiAurjs4_maths.js
+++ ChaiAurjs4_maths.js
@@ -17,18 +17,18 @@ const hundreds = 1000000
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
+ //console.log(Math.abs(-4));
+ //console.log(Math.round(4.6));
+ //console.log(Math.ceil(3.2));
 // console.log(Math.floor(4.9));
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
-const min = 10
-const max = 20
+// const min = 10
+// const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
